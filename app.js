5cf3ef36e38421723e98ce8f3e41a9475016dafb
TOMO-YOSHI <lucky.paddy10@gmail.com>
2022-03-30 17:05:04 -0700

    finish section2

--- app.js
+++ app.js
@@ -7,7 +7,30 @@ const app = Vue.createApp({
             title: 'The Final Empire',
             // titles: ['The Final Empire', 'something'],
             author: 'Brandon Sanderson',
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0,
+            books: [
+                {
+                    title: 'name of wind',
+                    author: 'patrick rothfuss',
+                    img: './assets/1.jpg',
+                    isFav: true
+                },
+                {
+                    title: 'the way of kings',
+                    author: 'Brandon Sanderson',
+                    img: './assets/2.jpg',
+                    isFav: false
+                },
+                {
+                    title: 'The Final Empire',
+                    author: 'Brandon Sanderson',
+                    img: 'assets/3.jpg',
+                    isFav: true
+                },
+            ],
+            url: 'https://google.com'
         }
     },
     methods: {
@@ -18,6 +41,25 @@ const app = Vue.createApp({
         },
         toggleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type);
+            if(data) {
+                console.log(data);
+            }
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
+        },
+        toggleFav(book) {
+            book.isFav = !book.isFav
+            console.log("click")
+        },
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter((book) => book.isFav)
         }
     }
 });
